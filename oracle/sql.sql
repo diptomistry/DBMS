70e61702f5109196f040d10a7ad14be29a3f61c9
Diptajoy Mistry <111699492+diptomistry@users.noreply.github.com>
2023-11-10 01:12:36 +0600

    Create sql.sql

---
+++ oracle/sql.sql
@@ -0,0 +1,10 @@
+CREATE TABLE universities (
+    university_id NUMBER PRIMARY KEY,
+    university_name VARCHAR2(255),
+    founded_date DATE,
+    location VARCHAR2(255),
+    website VARCHAR2(255),
+    ranking_2023 NUMBER,
+    type VARCHAR2(20),
+    QSWorldRanking_2023 VARCHAR2(20)
+);
