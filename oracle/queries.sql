3ed3525e54a1bf52c18ee5686305dbb752b8a543
Diptajoy Mistry <111699492+diptomistry@users.noreply.github.com>
2023-11-12 21:38:27 +0600

    Update queries.sql

--- oracle/queries.sql
+++ oracle/queries.sql
@@ -1,5 +1,11 @@
---1)To find the number of alumni of a particular university lives in a particular location
+--1)To find the number of alumni of a particular university live in a particular location
 SELECT COUNT(*) AS usa_alumni_count
+FROM alumni a
+JOIN universities u ON a.university_id = u.university_id
+WHERE a.location = 'USA' 
+AND u.university_name = 'University of Dhaka';
+--1.1)to have their details:
+SELECT a.full_name, a.Current_Employer, a.Current_Designation, a.batch
 FROM alumni a
 JOIN universities u ON a.university_id = u.university_id
 WHERE a.location = 'USA' 
